67f644370edd9518c3b7086a758ab805317ba7b0
Brian Espinosa <b@bje.co>
2023-10-09 15:47:27 -0700

    chore: move all API calls out to new subdomain

--- libs/components/src/lib/Main/SWRProvider.tsx
+++ libs/components/src/lib/Main/SWRProvider.tsx
@@ -6,13 +6,21 @@ export interface SWRProviderProps {
   children?: React.ReactNode;
 }
 
+// TODO: Move this to a reusable location
+const API_BASE_URL =
+  process.env.NODE_ENV === 'production'
+    ? 'https://api.pnwarmory.com'
+    : 'http://localhost:3333';
+
 export default function SWRProvider({ children, ...rest }: SWRProviderProps) {
   return (
     <SWRConfig
       {...rest}
       value={{
         fetcher: (resource, init) =>
-          fetch(resource, init).then((res) => res.json()),
+          fetch(new URL(resource, API_BASE_URL).href, init).then((res) =>
+            res.json(),
+          ),
       }}
     >
       {children}
