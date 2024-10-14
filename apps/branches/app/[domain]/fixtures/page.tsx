3836792eca42948a1db24d15ec760dd3f4adf5ca
Brian Espinosa <b@bje.co>
2024-10-14 13:48:27 -0700

    feat: scroll next game into view for fixtures

--- apps/branches/app/[domain]/fixtures/page.tsx
+++ apps/branches/app/[domain]/fixtures/page.tsx
@@ -1,15 +1,23 @@
 import { FixtureCard } from '@arsenalamerica/components';
 
-import { getFixtures } from '../../../endpoints/fixtures';
+import { getFixtures, getNextFixture } from '../../../endpoints/fixtures';
 
 export default async function FixturesPage() {
   const upcomingFixtures = await getFixtures();
+  const [{ id: nextFixtureId }] = await getNextFixture();
 
   return (
     <>
       {upcomingFixtures?.map((fixture) => {
-        const { id } = fixture;
-        return <FixtureCard key={id} {...fixture} />;
+        const { id, ...restFixture } = fixture;
+        const isNext = id === nextFixtureId;
+        return (
+          <FixtureCard
+            key={id}
+            id={isNext ? 'next-fixture' : undefined}
+            {...restFixture}
+          />
+        );
       })}
       {/* <code>
         <pre>{JSON.stringify(upcomingFixtures, null, 2)}</pre>
