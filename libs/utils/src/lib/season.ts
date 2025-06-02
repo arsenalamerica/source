69174c26524dc23144e901754c9ad40ec84807da
Brian Espinosa <b@bje.co>
2025-06-02 08:49:40 -0700

    fix: Season cut-over happens sooner

--- libs/utils/src/lib/season.ts
+++ libs/utils/src/lib/season.ts
@@ -3,12 +3,12 @@ const d = new Date();
 const currentMonth = d.getMonth();
 const currentYear = d.getFullYear();
 
-// NOTE: currentMonth starts at index 0. Switch the season over in June (index 5)
-const seasonYearStart = currentMonth > 4 ? currentYear : currentYear - 1;
+// NOTE: currentMonth starts at index 0. Switch the season over in August (index 7)
+const seasonYearStart = currentMonth > 6 ? currentYear : currentYear - 1;
 const seasonYearEnd = seasonYearStart + 1;
 
 const seasonStartDate = `${seasonYearStart}-07-01`;
-const seasonEndDate = `${seasonYearEnd}-05-31`;
+const seasonEndDate = `${seasonYearEnd}-06-30`;
 
 export const season = {
   start: seasonStartDate,
