5f277a65214deb36ff49562127f5c139a3534ce8
Brian Espinosa <b@bje.co>
2025-08-15 16:37:10 -0700

    fix: use default Next revalidate

--- apps/branches/endpoints/_api.ts
+++ apps/branches/endpoints/_api.ts
@@ -3,13 +3,13 @@ import wretch from 'wretch';
 import { API_BASE_URL } from '@arsenalamerica/utils';
 
 export const api = wretch(API_BASE_URL, {
-  next: { revalidate: 300 },
+  // next: { revalidate: 300 },
 }) // `revalidate` is a Next.js feature, revalidate every 5 minutes
   .errorType('json')
   .resolve((r) => r.json());
 
 export const apiLive = wretch(API_BASE_URL, {
-  next: { revalidate: 5 },
+  // next: { revalidate: 5 },
 }) // `revalidate` is a Next.js feature, revalidate every 30 seconds
   .errorType('json')
   .resolve((r) => r.json());
